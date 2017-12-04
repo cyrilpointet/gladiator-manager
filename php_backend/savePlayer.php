2a23d62c8ba119c42ccd87723fec490bf0d7f3c3
cyrilpointet <pointet.cyril@gmail.com>
2017-12-04 13:11:22 +0100

    quick save 2

--- php_backend/savePlayer.php
+++ php_backend/savePlayer.php
@@ -1,12 +1,10 @@
 <?php 
 
 // récupère la valeur 'player' de l'array $_POST
-$datas =$_POST['player'];
+$savedPlayer =$_POST['player'];
 $name = $_POST['name'];
 $password=$_POST['password'];
 
-$datasDecoded= json_decode($datas);
-$savedPlayer=$datasDecoded->{'player'};
 $fileName=$name.$password.".txt";
 
 // create file if it doesn't exist
@@ -20,11 +18,11 @@ fseek($savesFile, 0);
 $return=fgets($savesFile);
 
 //test result
-if ($return=='') {
+if ($return) {
    echo $return;
 } else {
     fseek($savesFile, 0);
-    fputs($savedPlayer);
+    fputs($savesFile, $savedPlayer);
     echo 'save ok';
 }
 
